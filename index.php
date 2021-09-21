347054eb7cb3c2018c123f03c6caea27f2a1b660
hilalahmad32 <hakofficial05@gmail.com>
2021-09-21 11:42:58 +0500

    html to pdf

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Html to pdf</title>
+</head>
+<style>
+    .container{
+        display: flex;
+        justify-content: center;
+        text-align: center;
+
+    }
+    .content h1{
+        font-family: cursive;
+    }
+    a{
+        background-color: lightgreen;
+        text-decoration: none;
+        padding: 10px 20px;
+        color: black;
+        font-family: cursive;
+    }
+    a:hover{
+        background-color: rgb(47, 209, 47);
+    }
+</style>
+<body>
+    <div class="container">
+        <div class="content">
+            <h1>Html to Pdf</h1>
+            <a href="dom.php">Donload in pdf</a>
+        </div>
+    </div>
+</body>
+</html>
