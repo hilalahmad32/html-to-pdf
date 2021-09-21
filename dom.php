f4fe58065eba99c88e2beb45b794783a2cc3527a
Hilal ahmad <86137247+hilalahmad32@users.noreply.github.com>
2021-09-21 12:51:04 +0500

    change in  dom.php

--- dom.php
+++ dom.php
@@ -1,16 +1,10 @@
 <?php
-// use Dompdf\Options;
 use Dompdf\Dompdf;
 
 require "vendor/autoload.php";
 try {
-// $option=new Options();
-// $option->set("defaultFont","Time New Roman");
 
 $dompdf=new Dompdf();
-$page=file_get_contents("index.html");
-// $dompdf->loadHtml($page);
-
 $conn=mysqli_connect("localhost","root","","testing");
 $sql="SELECT * FROM category";
 $run_sql=mysqli_query($conn,$sql);
@@ -22,11 +16,7 @@ if(mysqli_num_rows($run_sql) > 0){
         ";
     }
 }
-// $option=$dompdf->getOptions();
-// $option->setDefaultFont("Cambria, Cochin, Georgia, Times, 'Times New Roman', serif");
-// $dompdf->setOptions($option);
 $dompdf->loadHtml($output);
-// $dompdf->setPaper("A4",'portrait');
 $dompdf->setPaper("A4",'landscape');
 $dompdf->render();
 $dompdf->stream();
